ec5cd829db15b0b6a94f0b335c0c9b1d452e2670
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-01-25 17:36:10 -0200

    Adds end point show of todo_lists/ID

--- app/controllers/api/v1/todo_lists_controller.rb
+++ app/controllers/api/v1/todo_lists_controller.rb
@@ -1,14 +1,24 @@
 module Api
   module V1
     class TodoListsController < ApplicationController
+      before_action :set_todo_list, only: [:show]
 
       # GET /todo_lists
       def index
         @todo_lists = TodoList.all
-    
         render json: @todo_lists
       end
 
+      # GET /todo_lists/1
+      def show
+        render json: @todo_list
+      end
+
+      private
+
+      def set_todo_list
+        @todo_list = TodoList.find(params[:id])
+      end      
     end
   end
 end
