ff871d96b8f5a11f2c88fea9bc15345942923d99
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-01-26 16:03:19 -0200

    Adds ApplicationController#collect_metrics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   include JSONErrors
+  around_action :collect_metrics
+
+  def collect_metrics
+    start = Time.now
+    yield
+    duration = Time.now - start
+    Rails.logger.info "#{controller_name}##{action_name}: #{duration}s"
+  end
 end
