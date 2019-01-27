42c97c1ff55dd0ba38c368f09bb22b41fa4c3813
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-01-27 20:24:04 -0200

    Creates qt_controllers and qt_acition scopes of Metric

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,3 +1,5 @@
 class Metric < ApplicationRecord
-  scope :ordered,    -> { order(created_at: :desc) }
+  scope :ordered,        -> { order(created_at: :desc) }
+  scope :qt_controllers, -> { Metric.group(:controller_name).count }
+  scope :qt_action,      -> { Metric.group(:action_name).count }
 end
