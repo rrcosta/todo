d5f42bc9db83a18f3bc472cf7297e9c94e2df3c7
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-01-27 18:26:44 -0200

    Adds metric params coverage test

--- spec/controllers/metrics_controller_spec.rb
+++ spec/controllers/metrics_controller_spec.rb
@@ -19,4 +19,21 @@ RSpec.describe MetricsController, type: :controller do
       expect(response).to be_successful
     end
   end
+
+  describe 'Metric Params' do
+    context "with valid params" do
+      let(:metric_params) { FactoryBot.attributes_for(:metric) }
+
+      it 'creates a new metric' do
+        expect(metric_params).to eql(
+          action_name: "view",
+          controller_name: "TodoList",
+          db_runtime: 11.3,
+          time_process: 28.5,
+          view_runtime: 17.2,
+        )
+
+      end
+    end
+  end
 end
