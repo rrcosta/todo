635b5913be0805c0ba895f9a067412ab270dc6b0
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-01-27 22:39:04 -0200

    Adds scopes of average acess view  and acess bd

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -4,4 +4,6 @@ class Metric < ApplicationRecord
   scope :qt_action,      -> { Metric.group(:action_name).count }
   scope :sum_tot_time,   -> { Metric.sum(:time_process) }
   scope :average_of_time,-> { Metric.average("time_process") }
+  scope :average_of_view,-> { Metric.average("view_runtime") }
+  scope :average_of_bd,  -> { Metric.average("db_runtime") }
 end
