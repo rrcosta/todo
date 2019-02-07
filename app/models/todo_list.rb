e05e28381d8594c422e1030c6510c7cfe2b2db1a
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-02-07 11:37:23 -0200

    Creates group_by_status scopes

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,6 +1,7 @@
 class TodoList < ApplicationRecord
   validates :title, :description, presence: true
 
-  scope :pending, -> { where(status: false) }
-  scope :completed, -> { where(status: true) }
+  scope :pending        , -> { where(status: false) }
+  scope :completed      , -> { where(status: true) }
+  scope :group_by_status, -> { TodoList.group(:status).count }
 end
